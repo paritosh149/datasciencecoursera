caf0a76b1ff36fb999ccdc44b12a1c868766b666
paritosh149 <paritosh149@outlook.com>
2014-09-14 01:55:34 +0530

    Programming Assignment 1 - Part 1
    Part 1

---
+++ pollutantmean.R
@@ -0,0 +1,31 @@
+pollutantmean <- function(directory, pollutant, id=1:332){
+  if(file.exists(directory))
+  {
+    ## print(paste(directory,'exists.',sep=" "))
+    MF <- matrix(,0,4)
+    dimnames(MF)[[2]] <- list('Date','sulfate','nitrate','ID')
+    for(filename in id)
+    {
+      filename <- sprintf('%03d',filename)
+      ##print(filename)
+      fullfilename <- paste(directory,"/",filename,".csv",sep="")
+      if(file.exists(fullfilename))
+      {
+         ## print(paste(fullfilename,"exists",sep=" "))
+         tab01 <- read.csv(fullfilename)
+         MF <- merge(MF,tab01,all.x=TRUE,all.y=TRUE)
+      }
+      else
+      {
+        print(paste(fullfilename,"does not exists",sep=" "))
+      }
+    }
+     ## print(MF[pollutant])
+    meanvalue <- round(mean(MF[[pollutant]], na.rm=TRUE),digits=3)
+    ## print(meanvalue)
+  }
+  else
+  {
+    print(paste(directory,"does not exists.",sep=" "))
+  }
+}
